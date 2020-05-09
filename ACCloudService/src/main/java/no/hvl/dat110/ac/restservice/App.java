63b38cc375f3c5590e6980dfac76149a3d30a13b
Andrine <Andrine@192.168.38.109>
2020-05-09 12:20:19 +0200

    Done

--- ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/App.java
+++ ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/App.java
@@ -8,6 +8,7 @@ import static spark.Spark.post;
 import static spark.Spark.delete;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 /**
  * Hello world!
@@ -46,6 +47,54 @@ public class App {
 		// TODO: implement the routes required for the access control service
 		// as per the HTTP/REST operations describined in the project description
 		
+		post("accessdevice/log", (req, res) -> {
+			Gson gson = new Gson();
+			
+			//JsonObject jsonObj = gson.fromJson(req.body(), JsonObject.class);
+			//String message = jsonObj.get("message").getAsString();
+			
+			AccessMessage message = gson.fromJson(req.body(), AccessMessage.class);
+
+			int id = accesslog.add(message.getMessage());
+
+			return gson.toJson(accesslog.get(id));
+		});
+
+		get("/accessdevice/log/:id", (req, res) -> {
+			return accesslog.toJson();
+		});
+
+		get("/accessdevice/log/:id", (req, res) -> {
+			int id = -1;
+			try {
+				id = Integer.parseInt(req.params(":id"));
+			} catch (Exception e) {
+				System.out.println("Invalid id");
+			}
+			Gson gson = new Gson();
+
+			return gson.toJson(accesslog.get(id));
+		});
+
+		put("accessdevice/code", (req, res) -> {
+			Gson gson = new Gson();
+			AccessCode accessCode = gson.fromJson(req.body(), AccessCode.class);
+			accesscode.setAccesscode(accessCode.getAccesscode());
+
+			return req.body();
+		});
+
+		get("accessdevice/code", (req, res) -> {
+			Gson gson = new Gson();
+
+			return gson.toJson(accesscode);
+		});
+
+		delete("/accessdevice/log", (req, res) -> {
+			accesslog.clear();
+
+			return accesslog.toJson();
+		});
     }
     
 }
